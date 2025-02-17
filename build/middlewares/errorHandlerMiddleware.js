c9f0989fd4831903992f3bbf610cab2f64a2b14d
eabasguliyev <elgun.abasquliyev@hotmail.com>
2025-02-17 12:01:03 +0400

    'updates'

---
+++ build/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errorHandlerMiddleware = void 0;
+const CustomError_1 = require("../errors/CustomError");
+const errorHandlerMiddleware = (err, req, res, next) => {
+    if (err instanceof CustomError_1.CustomError) {
+        res.status(err.statusCode).send({ errors: err.serializeErrors() });
+        return;
+    }
+    res.status(500).send({
+        errors: [{ message: "Something went wrong" }],
+    });
+};
+exports.errorHandlerMiddleware = errorHandlerMiddleware;
+//# sourceMappingURL=errorHandlerMiddleware.js.map
